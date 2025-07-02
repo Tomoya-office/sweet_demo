4c6d060ba0f1d268091c1a367ea9565aead94131
kanakoAzm <knka0717@gmail.com>
2025-07-02 22:30:59 +0900

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import styles from "./page.module.css";
+import { Header } from "@/components/ui/Header";
+import { Footer } from "@/components/ui/Footer";
+
+
+export default function Home() {
+  return (
+  <>
+  <Header />
+    <main className={styles.main}>
+    </main>
+  <Footer/>
+  </>
+  );
+}
