92046ecd0c4bcd78118acd51b1c51da2b6d2267a
kanakoAzm <knka0717@gmail.com>
2025-08-27 21:46:15 +0900

    add:Menuコンポーネント実装

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import { Footer } from "@/components/ui/Footer";
 import { Story } from "@/components/ui/Story";
 import { News } from "@/components/ui/News";
 import { Shop } from "@/components/ui/Shop";
+import { Menu } from "@/components/ui/Menu";
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <Story />
       <News />
       <Shop bgImage="/shop_photo/shop_image1.png"></Shop>
+      <Menu />
       <Shop bgImage="/shop_photo/shop_image2.png"></Shop>
     </main>
   <Footer/>
---
+++ src/components/ui/Menu/index.js
@@ -0,0 +1,130 @@
+import styles from './index.module.css'
+import Image from "next/image";
+
+export const Menu = () => {
+  return (
+    <section className={styles.menu}>
+      <div className={styles.wrap}>
+        <h3 className={styles.title}>~Menu~</h3>
+        <div>
+          <div className={styles['info-wrap']}>
+            <Image
+              className={styles.shortcake}
+              src="/menu_photo/shortcake.png"
+              alt="ショートケーキ"
+              width={635}
+              height={490}
+            />
+            <div className={`${styles['cake-info']} ${styles['shortcake-info']}`}>
+              <p className={styles['cake-title']}>ふんわり贅沢なショートケーキ！</p>
+              <p className={styles['cake-content']}>ふわふわのスポンジに、甘さ控えめの生クリームと新鮮ないちごを<br />サンドした王道のショートケーキ。<br />やさしい甘さといちごの酸味が口いっぱいに広がります。<br />ティータイムやお祝い、自分へのご褒美にぴったりの一品です。</p>
+              <table className={styles.table}>
+                <tbody>
+                  <tr>
+                    <th className={styles.header}>値段</th>
+                    <td className={styles.data}>520円</td>
+                  </tr>
+                  <tr>
+                    <th className={styles.header}>カロリー</th>
+                    <td className={styles.data}>約372kcal</td>
+                  </tr>
+                  <tr>
+                    <th className={styles.header}>アレルギー</th>
+                    <td className={styles.data}>卵・乳成分・小麦・ゼラチン・いちご</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className={styles['info-wrap']}>
+            <Image
+              className={styles.cheesecake}
+              src="/menu_photo/cheesecake.png"
+              alt="チーズケーキ"
+              width={685}
+              height={314}
+            />
+            <div className={`${styles['cake-info']} ${styles['cheesecake-info']}`}>
+              <p className={styles['cake-title']}>クリーミーな極上チーズケーキ！</p>
+              <p className={styles['cake-content']}>濃厚でなめらかな口どけが魅力の特製チーズケーキ。<br />厳選したクリームチーズのコクと、ほどよい酸味が絶妙にマッチ。<br />タルトのサクサク感とともに、贅沢な味わいをお楽しみいただけます。<br />ひとくちごとに広がる深いコクが、心まで満たしてくれる一品です。</p>
+              <table className={styles.table}>
+                <tbody>
+                  <tr>
+                    <th className={styles.header}>値段</th>
+                    <td className={styles.data}>480円</td>
+                  </tr>
+                  <tr>
+                    <th className={styles.header}>カロリー</th>
+                    <td className={styles.data}>約362kcal</td>
+                  </tr>
+                  <tr>
+                    <th className={styles.header}>アレルギー</th>
+                    <td className={styles.data}>卵・乳成分・小麦</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className={styles['info-wrap']}>
+            <Image
+              className={styles.chocolatecake}
+              src="/menu_photo/chocolatecake.png"
+              alt="チョコレートケーキ"
+              width={695}
+              height={484}
+            />
+            <div className={`${styles['cake-info']} ${styles['chocolatecake-info']}`}>
+              <p className={styles['cake-title']}>濃厚×爽やか 至福のチョコケーキ！</p>
+              <p className={styles['cake-content']}>いちごの甘酸っぱさと、とろける濃厚チョコが奏でる贅沢なハーモニー。<br />ふんわりチョコスポンジに、自家製ガナッシュとフレッシュいちごを たっぷりトッピング。<br /> 華やかな見た目で、パーティーやご褒美にもぴったり。<br />今だけの特別な味わいを、心まで甘く染まるひとときとともに。</p>
+              <table className={styles.table}>
+                <tbody>
+                  <tr>
+                    <th className={styles.header}>値段</th>
+                    <td className={styles.data}>520円</td>
+                  </tr>
+                  <tr>
+                    <th className={styles.header}>カロリー</th>
+                    <td className={styles.data}>約372kcal</td>
+                  </tr>
+                  <tr>
+                    <th className={styles.header}>アレルギー</th>
+                    <td className={styles.data}>卵・乳成分・小麦・ゼラチン・いちご</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className={styles['info-wrap']}>
+            <Image
+              className={styles.canele}
+              src="/menu_photo/canele.png"
+              alt="カヌレ"
+              width={670}
+              height={427}
+            />
+            <div className={`${styles['cake-info']} ${styles['canele-info']}`}>
+              <p className={styles['cake-title']}>濃厚バニラ香る、カヌレ</p>
+              <p className={styles['cake-content']}>外はカリッと香ばしく、中はもっちりしっとり。<br /> ラム酒とバニラがふわりと香る、奥深い味わいのフランス伝統菓子。<br /> ひとくちで広がる甘さと香りが、日常にささやかな贅沢を添えます。<br />コーヒーや紅茶と一緒に、心とろけるひとときをどうぞ。</p>
+              <table className={styles.table}>
+                <tbody>
+                  <tr>
+                    <th className={styles.header}>値段</th>
+                    <td className={styles.data}>320円</td>
+                  </tr>
+                  <tr>
+                    <th className={styles.header}>カロリー</th>
+                    <td className={styles.data}>約172kcal</td>
+                  </tr>
+                  <tr>
+                    <th className={styles.header}>アレルギー</th>
+                    <td className={styles.data}>卵・乳成分・小麦・ラム酒使用</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    )
+}
